d23e084634b72c4f60c351cf1b430d0e80084cbf
BiteSimao <gabriel_l_simao@estudante.sesisenai.org.br>
2026-04-01 13:31:13 -0300

    100% atualizado até o dia 01/04

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-#CRIAÇÃO E CONFIGURAÇÃO DO BANCO DE DADOS SQLITE
+#CRIAÇÃO E CONFIGURAÇÃO DO BANCO DE DADO SQLITE
 import sqlite3
 
 #Constante com o nome do arquivo de banco de dados
@@ -6,44 +6,48 @@ import sqlite3
 bd_ordem = 'ordens.bd'
 
 def get_connection():
+    '''
+    Cria e retorna uma conexão com o banco de dados SQLite
     
-    '''  Cria e retorna uma conexão com o banco de dados SQLite
+    A propriedade row_factory permite acessar as colunas pelo nome
+    (Ex: ordem['produto'] em vez de pelo índice(Ex:ordem[1]))
     
-     A propriedade row_factory permite acessar as colunas pelo nome
-     (Ex: Ordem['produto] em vez de pelo índice(Ex:ordem[1]))
-    
-     Retorna:
-        sqlite3.Connection: Objeto de conexão com o banco de dados.'''
-    
+    Retorna:
+        sqlite3.Connection: objeto de conexão com o banco de dados.
+    '''
     conn = sqlite3.connect(bd_ordem)
     conn.row_factory = sqlite3.Row
     return conn
 
 def init_bd():
     '''
-    # Inicializar o banco de dados criando a tabela 'ordens' se ela ainda não existir. Seguro para chamar múltiplas vezes.
+    Inicializa o banco de dados criando a tabela 'ordens' se ela ainda não
+    existir. Seguro para chamar múltiplas vezes.
     '''
     conn = get_connection()
     
-    #Cursor() permitir executar comandos SQL
+    # Cursor() permitir executar comandos SQL
     cursor = conn.cursor()
     
-    #IF NOT EXIST garantir que o comando não falhe se a tabela já existir
+    #IF NOT EXIST garantir que o comando não falhe se a tabela já exixtir
     
     cursor.execute('''
-                   CREATE TABLE IF NOT EXISTS ordens (
-                       id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                   CREATE TABLE IF NOT EXISTS ordens(
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
                        produto      TEXT        NOT NULL,
                        quantidade   INTEGER     NOT NULL,
                        status       TEXT        DEFAULT 'Pendente',
-                       criado_em    TEXT        DEFAULT(datatime('now', 'localtime'))
+                       criado_em    TEXT        DEFAULT(datetime('now','localtime'))
                        )
                        ''')
     
-    #Commit() Salvar alterações no arquivo .bd
+    #commit() Salvar as alterações no arquivo .bd
     conn.commit()
-
-    #Close() Liberar conexão (Boa Prática)
+    
+    #close() liberar a conexão(boa prática)
     conn.close()
+    
+    print("Banco de dados inicializado com sucesso!")
 
-print("Banco de Dados Inicializado com Sucesso!")
+
+init_bd()
